0b45f9961d9ad9767cc88e41e6d47bb3858d41ce
Ivan A-R <ivan@tuxotronic.org>
2018-05-23 10:12:47 +0300

    Display flashing report

--- apps/libopencm3.rules.mk
+++ apps/libopencm3.rules.mk
@@ -240,14 +240,14 @@ ifeq ($(OOCD_FILE),)
 		$(OOCD) -f interface/$(OOCD_INTERFACE).cfg \
 		-f target/$(OOCD_TARGET).cfg \
 		-c "program $(*).elf verify reset exit" \
-		$(NULL)
+		# $(NULL)
 else
 %.flash: %.elf
 	@printf "  FLASH   $<\n"
 	(echo "halt; program $(realpath $(*).elf) verify reset" | nc -4 localhost 4444 2>/dev/null) || \
 		$(OOCD) -f $(OOCD_FILE) \
 		-c "program $(*).elf verify reset exit" \
-		$(NULL)
+		# $(NULL)
 endif
 else
 %.flash: %.elf
